617df81bcb211ed2c1af037265dc1a8f4b85d621
Kevin Lu <kevinlu1248@gmail.com>
2020-03-20 22:42:56 -0700

    Added Coronavirus banner (#57)

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
+    $("body").append(`
+        <div id="alert" class="alert alert-warning alert-dismissible fade show fixed-bottom" role="alert">
+            <strong>COVID-19 Update:</strong> Hackcouver 2020 will be postponed to late June or early May. The specific 
+            date will come shortly and further information can be found at 
+            <a href="http://bit.ly/Hackcouver-COVID19">
+                http://bit.ly/Hackcouver-COVID19
+            </a>.
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    `);
     $("a").click(function() {
         // console.log(this);
         // TODO: make it scroll up after clicking anchor tag
